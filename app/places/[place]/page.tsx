1413452c4ee2982c88ad7a825222be82db5cde40
exalted100 <harounayomikun@gmail.com>
2025-01-03 00:56:13 +0000

    add pictures

--- app/places/[place]/page.tsx
+++ app/places/[place]/page.tsx
@@ -25,8 +25,8 @@ export default async function Place({
   }
 
   return (
-    <div className="px-5 lg:px-20 mb-6 lg:mb-20 font-JetbrainsMono text-base lg:text-xl max-w-screen-2xl xl:mx-auto">
-      <h1 className="sticky w-100 pt-10 lg:pt-24 top-0 text-5xl lg:text-8xl font-JimNightshade mb-10" style={{backgroundColor: "#D5B59C"}}>
+    <div className="px-5 lg:px-20 mb-6 lg:mb-20 font-JetbrainsMono text-base max-w-screen-2xl xl:mx-auto">
+      <h1 className="sticky w-100 pt-10 lg:pt-24 top-0 text-5xl font-JimNightshade mb-10" style={{backgroundColor: "#D5B59C"}}>
         Postcard from {placesData[currentIndex].title}
       </h1>
 
