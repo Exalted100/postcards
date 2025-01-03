601a79f0cee03858859752d020c92df9af984419
exalted100 <harounayomikun@gmail.com>
2025-01-03 16:10:32 +0000

    add lakowe story

--- app/data.tsx
+++ app/data.tsx
@@ -6,9 +6,9 @@ export const placesData = [
         country: "Nigeria",
         info: [],
         content: [ {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
+            paragraphs: ['Lakowe Lakes is a Golf and Country Estate in Lagos, and I was there between the second and sixth of December. It made me wonder about the difference between a pond and a lake. I had always thought of lakes as very large water bodies, possibly farther than I could see. Apparently not always. There is no clear distinction, but most sources say ponds are small and shallow and lakes are wider and have more depth. Some of the lakes in Lakowe did not appear shallow.',
+                'It wasn\'t as windy as I would have expected. There is a lot of greenery and nature. I\'ve come to expect those sorts of places to be windy and cool. Instead, the air was soft and the sun shone hot. To be fair, the Estate is not very far from the Lekki Epe express way, which has plenty of footprint and emissions.',
+                'It was a nice place to stay. It wasn\'t far - I left from Ikeja and it was a roughly two hour drive. But it also gave the escape vibe, like it was away from the hustle and bustle of Lagos.'
             ],
             image: {
                 url: '/lake-in-field.jpg',
@@ -16,9 +16,9 @@ export const placesData = [
             },
         },
         {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
+            paragraphs: ['I really liked this tree island thing. Most of the lakes were relatively large, uniterrupted pools of water. I some had leaves on their surfaces. One had a duck that often sat close to its edge. But this cluster of trees was the largest and most decorative thing I saw. It looked pretty.',
+                'There wasn\'t a very diverse wildlife present. There were many trees and grasses, lawns, some monkeys, I saw a duck, and likely some frogs. Maybe deeper in the jungles, there were. But I saw very little over the few days I spent there. No dogs, no cats, no settled birds. I think those would have been nice to see.',
+                'I suspect there was pollen in the air. And I suspect I am allergic to them. Hours after I arrived, I had a minor headache and a catarrh. I first thought it was a cold, because I wasn\'t alone in it. But then, it left hours after I left. So I\'m left to suspect that it was a pollen allergy. I should have taken some medicine for that.'
             ],
             image: {
                 url: '/trees-in-pond.jpg',
@@ -26,9 +26,9 @@ export const placesData = [
             },
         },
         {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
+            paragraphs: ['The food! The food! I really liked my meals.',
+                'Every meal was a three course buffet. So there was much optionality across apptizers, main courses, and desserts. The meals were fresh and well made, and so I didn\'t worry about whether they would taste good. Or what meal was least likely to be cooked wrong. Instead, I could focus on what I wanted.',
+                'One afternoon, I didn\'t feel like eating much. I wasn\'t hungry and had breakfast a couple of hours before. I settled on a plate of goat meat peppersoup. Whoever came up with peppersoup was in their bag. They cooked, literally.'
             ],
             image: {
                 url: '/lakowe-peppersoup.jpg',
@@ -36,103 +36,9 @@ export const placesData = [
             },
         },
         {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
-            ],
-            image: {
-                url: '/beach-bonfire.jpg',
-                alt: 'Bonfire on a beach at night',
-            },
-        }, ],
-    },
-    {
-        slug: "lakowe",
-        title: "Lakowe",
-        city: "Lagos",
-        country: "Nigeria",
-        info: [],
-        content: [ {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
-            ],
-            image: {
-                url: '/lake-in-field.jpg',
-                alt: 'Lake in the midst of field',
-            },
-        },
-        {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
-            ],
-            image: {
-                url: '/trees-in-pond.jpg',
-                alt: 'Tree Island in a pond',
-            },
-        },
-        {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
-            ],
-            image: {
-                url: '/lakowe-peppersoup.jpg',
-                alt: 'Lakowe Goat meat peppersoup',
-            },
-        },
-        {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
-            ],
-            image: {
-                url: '/beach-bonfire.jpg',
-                alt: 'Bonfire on a beach at night',
-            },
-        }, ],
-    },
-    {
-        slug: "lakowe",
-        title: "Lakowe",
-        city: "Lagos",
-        country: "Nigeria",
-        info: [],
-        content: [ {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
-            ],
-            image: {
-                url: '/lake-in-field.jpg',
-                alt: 'Lake in the midst of field',
-            },
-        },
-        {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
-            ],
-            image: {
-                url: '/trees-in-pond.jpg',
-                alt: 'Tree Island in a pond',
-            },
-        },
-        {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
-            ],
-            image: {
-                url: '/lakowe-peppersoup.jpg',
-                alt: 'Lakowe Goat meat peppersoup',
-            },
-        },
-        {
-            paragraphs: ['London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.',
-                'London is an interesting place. London is diverse. It has been home to the Romans, the Vikings, the Brits, the Saxons, the English, and many more. And as things worsen in Nigeria, many Nigerians, like me, now come to London.'
+            paragraphs: ['This was my first time around a bonfire. And I know that\'s surprising, even to me. Because I got more opportunities to be around a bonfire in the past. But I never really took any of those chances, until now. And a fire can really preface fun. If you doubt me, just ask Moses!',
+                'We mostly played games around the bonfire. There was a version of charades, where we had to describe the word. But we couldn\'t move parts of our bodies. And we tried to guess as many words as we could within a minute. Two teams took turns until a team got to 20. My team lost.',
+                'There was also a version of uno. I think the name had "hood" or "ghetto" in it, and there was a bitch slap card, but I now struggle to remember the name of the game. That was really fun.'
             ],
             image: {
                 url: '/beach-bonfire.jpg',
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="px-5 lg:px-20 mb-6 lg:mb-20 font-JetbrainsMono text-base max-w-screen-2xl xl:mx-auto">
       <h1 className="sticky w-100 pt-10 lg:pt-24 top-0 text-5xl lg:text-6xl font-JimNightshade mb-10" style={{backgroundColor: "#D5B59C"}}>Postcards from Earth</h1>
 
-      <p className="mb-10 leading-12">I’m seeing places. And here, you can see them too. <br/>You can see places on this ball we call home, places I’ve been.</p>
+      <p className="mb-10 leading-12">I’m seeing places. Come see them too.</p>
 
       { placesData.map((placeData) => {
         return <><Link key={placeData.slug} className="underline italic mb-5 block" href={`/places/${placeData.slug}`}>{placeData.title}, {placeData.city} - {placeData.country}</Link></>
