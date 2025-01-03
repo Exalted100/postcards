1c48aa68888a7d4cae448d35d71320ec7ee19d53
exalted100 <harounayomikun@gmail.com>
2025-01-03 20:46:37 +0000

    typo fixes

--- app/data.tsx
+++ app/data.tsx
@@ -6,7 +6,7 @@ export const placesData = [
         country: "Nigeria",
         info: [],
         content: [ {
-            paragraphs: ['Lakowe Lakes is a Golf and Country Estate in Lagos, and I was there between the second and sixth of December. It made me wonder about the difference between a pond and a lake. I had always thought of lakes as very large water bodies, possibly farther than I could see. Apparently not always. There is no clear distinction, but most sources say ponds are small and shallow and lakes are wider and have more depth. Some of the lakes in Lakowe did not appear shallow.',
+            paragraphs: ['Lakowe Lakes is a Golf and Country Estate in Lagos, and I was there between the second and sixth of December, 2024. It made me wonder about the difference between a pond and a lake. I had always thought of lakes as very large water bodies, possibly farther than I could see. Apparently not always. There is no clear distinction, but most sources say ponds are small and shallow and lakes are wider and have more depth. Some of the lakes in Lakowe did not appear shallow.',
                 'It wasn\'t as windy as I would have expected. There is a lot of greenery and nature. I\'ve come to expect those sorts of places to be windy and cool. Instead, the air was soft and the sun shone hot. To be fair, the Estate is not very far from the Lekki Epe express way, which has plenty of footprint and emissions.',
                 'It was a nice place to stay. It wasn\'t far - I left from Ikeja and it was a roughly two hour drive. But it also gave the escape vibe, like it was away from the hustle and bustle of Lagos.'
             ],
@@ -16,7 +16,7 @@ export const placesData = [
             },
         },
         {
-            paragraphs: ['I really liked this tree island thing. Most of the lakes were relatively large, uniterrupted pools of water. I some had leaves on their surfaces. One had a duck that often sat close to its edge. But this cluster of trees was the largest and most decorative thing I saw. It looked pretty.',
+            paragraphs: ['I really liked this tree island thing. Most of the lakes were relatively large, uniterrupted pools of water. Some had leaves on their surfaces. One had a duck that often sat close to its edge. But this cluster of trees was the largest and most decorative thing I saw. It looked pretty.',
                 'There wasn\'t a very diverse wildlife present. There were many trees and grasses, lawns, some monkeys, I saw a duck, and likely some frogs. Maybe deeper in the jungles, there were. But I saw very little over the few days I spent there. No dogs, no cats, no settled birds. I think those would have been nice to see.',
                 'I suspect there was pollen in the air. And I suspect I am allergic to them. Hours after I arrived, I had a minor headache and a catarrh. I first thought it was a cold, because I wasn\'t alone in it. But then, it left hours after I left. So I\'m left to suspect that it was a pollen allergy. I should have taken some medicine for that.'
             ],
